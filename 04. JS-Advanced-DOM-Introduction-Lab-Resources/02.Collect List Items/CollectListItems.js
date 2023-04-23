393501f3ac41e34f3e970b8112f9735b2639d5e1
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-24 00:23:31 +0300

    Update CollectListItems.js

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,9 +1,27 @@
+// function extractText() {
+//     const text = document.getElementById('items').textContent
+
+//     const textArea = document.getElementById('result');
+//     textArea.value = text
+
+
+    
+// }
+
+
+
 function extractText() {
-    const text = document.getElementById('items').textContent
+    const text = document.getElementById('items');
+    const resultArr  = Array.from(text.children) ;
 
-    const textArea = document.getElementById('result');
-    textArea.value = text
+   let newResultArr = resultArr
+    .map( li => li.textContent)
+    .join('\n');
 
+    let textArea = document.getElementById('result');
+    textArea.value = newResultArr
+
+   
 
     
 }
