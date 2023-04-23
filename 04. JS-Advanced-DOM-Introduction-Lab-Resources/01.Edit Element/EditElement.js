62738f9f35812e88e1b06f283fa40f2eb9bcf997
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-23 23:36:28 +0300

    DOM lab introduction

---
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
@@ -0,0 +1,11 @@
+function editElement(element, match, replacer) {
+
+    const pattern = new RegExp(match, "g")
+    const text = element.textContent;
+
+    const result  = text.replace(pattern, replacer);
+
+    element.textContent = result;
+
+   // console.log(text);
+}
