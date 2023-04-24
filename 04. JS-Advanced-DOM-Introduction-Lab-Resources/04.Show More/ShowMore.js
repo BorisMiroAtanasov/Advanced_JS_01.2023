33e4d79469ea49f708f06b3f676958e3d726e999
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-24 23:10:16 +0300

    task 4

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
@@ -1,3 +1,29 @@
 function showText() {
-    // TODO
+
+    const hideText = document.getElementById('more');
+
+    const moreText = document.getElementById('text');
+
+
+
+   hideText.style.display = 'none'
+        moreText.style.display = 'inline'
+    
+
+    
 }
+
+/*
+function showText() {
+
+    const hideText = document.getElementById('more');
+
+    hideText.style.display = 'none'
+
+    const moreText = document.getElementById('text');
+        moreText.style.display = 'inline'
+    
+
+    
+}
+*/
