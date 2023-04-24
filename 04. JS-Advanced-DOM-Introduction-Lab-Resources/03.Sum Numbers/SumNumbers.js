67542222d0476e2276920a04da1b458be72422a4
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-24 22:42:58 +0300

    Update SumNumbers.js

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,13 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const firsNum = Number(document.getElementById('num1').value);
+    const secondNum = Number(document.getElementById('num2').value);
+
+    let result  = firsNum + secondNum
+    const sum = document.getElementById('sum');
+
+    sum.value = result;
+
+// console.log(firsNum);
+// console.log(secondNum);
+// console.log(result);  
 }
