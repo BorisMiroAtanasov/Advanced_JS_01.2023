845d47c8a09cde8eb266219387cda841f1c6d675
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-25 00:05:03 +0300

    task 6

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,34 @@
+
 function sumTable() {
+    const table = document.querySelectorAll('tbody tr');
+    const sum = document.getElementById('sum');
+
+
+    let total = 0;
+
+    for(let i=1 ; i< table.length ; i++){
+        let cols = table[i].children;
+        let price = cols[cols.length -1].textContent;
+
+        total += Number(price);
+
+    }
+    sum.textContent = total;
+
+
+
 
 }
+
+
+
+
+/*
+function sumTable() {
+    const price = document.querySelectorAll('tbody tr');
+
+    console.log(Array.from(price));
+
+}
+
+*/
