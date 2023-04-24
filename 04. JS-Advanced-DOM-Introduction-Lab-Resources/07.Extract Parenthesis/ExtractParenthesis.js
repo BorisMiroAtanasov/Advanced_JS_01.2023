f3d9c7a1fa146db8b87d707829f9e0d28f3080f9
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-25 00:45:24 +0300

    task 7

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,52 @@
+/*
 function extract(content) {
 
+    let param = document.getElementById(content);
+
+    let pattern = /\(([^)]+)\)/g;
+
+    let result = [];
+
+    let maches = param.textContent.matchAll(pattern);
+
+for (const match of maches) {
+    result.push(match[1])
+
+    
 }
+return result.join('; ')
+    
+
+}
+*/
+
+ /*
+  function extract(elementId) {
+  let para = document.getElementById(elementId).textContent;
+  let pattern = /\(([^)]+)\)/g;
+  let result = [];
+
+  let match = pattern.exec(para);
+  while(match) {
+    result.push(match[1]);
+    match = pattern.exec(para);
+  }
+
+  return result.join('; ');
+}
+*/
+
+
+function extract(content) {
+    let contentElement = document.getElementById(content)
+    let pattern = /\(([^)]+)\)/g;
+    let result = [];
+  
+    let matches = contentElement.textContent.matchAll(pattern)
+  for (const match of matches) {
+    result.push(match[1]);
+     
+  }
+  
+    return result.join('; ');
+  }
