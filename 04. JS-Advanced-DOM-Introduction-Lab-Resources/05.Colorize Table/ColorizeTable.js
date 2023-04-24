919459b2a7f242183bba913329b0b483a5b20a4c
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-24 23:43:56 +0300

    task  5

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,38 @@
 function colorize() {
-    // TODO
+
+    const colorized = document.querySelectorAll('table tr');
+    let index = 0;
+    for (const row of colorized) {
+        index++;
+        if(index % 2 == 0){
+            row.style.background = 'teal'
+        }
+    }
+
+    //colorized.style.background = 'teal'
+
+
+    
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+/* Not working
+
+function colorize() {
+
+    const colorized = document.querySelector('table tr:nth-child(even)');
+    colorized.style.background = 'teal'
+
+  
+}
+*/
