95252f0f2b7d414951e1b9c19aa5bbcbe9b551d1
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-26 00:25:36 +0300

    task 2

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById("text").value;
+  let naming = document.getElementById("naming-convention").value;
+  let result = "";
+  let resultContainer = document.getElementById("result");
+
+  if (naming == "Camel Case") {
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] == " ") {
+        result += text[i + 1].toUpperCase();
+        i++;
+      } else {
+        result += text[i].toLowerCase();
+      }
+    }
+
+    // console.log(text);
+  } else if (naming == "Pascal Case") {
+    result += text[0].toUpperCase();
+    for (let i = 1; i < text.length; i++) {
+      
+      if (text[i] == " ") {
+        result += (text[i + 1].toUpperCase());
+        i++;
+      } else {
+        result += text[i].toLowerCase();
+      }
+    }
+  } else {
+    result = "Error!";
+  }
+  resultContainer.textContent = result;
 }
