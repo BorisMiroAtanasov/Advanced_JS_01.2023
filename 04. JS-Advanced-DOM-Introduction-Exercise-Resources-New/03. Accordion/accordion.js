e226ebe9f0e3435dd6bfc53d9035de2a6ff0d36f
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-26 23:21:17 +0300

    task 3 Dom intro exersises

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,23 @@
 function toggle() {
-    console.log('TODO:...');
+    const btn = document.querySelector('.button');
+    let hidenText = document.getElementById('extra');
+
+    if(  btn.textContent === 'More'){
+        btn.textContent = 'Less';
+
+        hidenText.style.display = 'block'
+
+
+    } else if (btn.textContent === 'Less'){
+        btn.textContent = 'More';
+
+        hidenText.style.display = 'none'
+
+    }
+
+    
+    
+
+    //console.log(hidenText);
+
 }
