bfca1eb541a106475971192baa01ba57d3103738
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-26 23:58:03 +0300

    Update search.js
    task 4 complite

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+  const serchText = document.getElementById("searchText").value;
+  const towns = Array.from(document.querySelectorAll("#towns li"));
+  const resultElement = document.getElementById("result");
+  let count = 0;
+
+  for (const town of towns) {
+    if (town.textContent.includes(serchText)) {
+      count++;
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+    }else{
+      town.style.fontWeight = "normal";
+      town.style.textDecoration = "none";
+      
+    }
+
+  }
+
+  resultElement.textContent = `${count} matches found`;
+
 }
