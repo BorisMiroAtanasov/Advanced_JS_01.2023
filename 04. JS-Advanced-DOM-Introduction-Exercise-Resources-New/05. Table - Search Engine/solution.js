eb2684a31f1a0b00db52b27718ffc82ed527caae
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-27 00:46:51 +0300

    Update solution.js
    task 5 update solution - clear input field

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,18 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      let inputText = document.getElementById('searchField').value;
+      let inputText = document.getElementById('searchField');
       //const data = Array.from(document.querySelectorAll('.container tr'));
       let rows = document.querySelectorAll('tbody tr');
 
 
       for (const row of rows) {
          row.classList.remove('select');
-         if(inputText !== '' && row.innerHTML.includes(inputText)){
+         if(inputText.value !== '' && row.innerHTML.includes(inputText.value)){
             row.className = 'select'
          }
          
       }
+      inputText.value = '';
      
 
    }
