b4bc3d38e92dbbbd495ce5e997d4321c9d5b61aa
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-27 00:41:19 +0300

    Update solution.js
    task 5 done

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let inputText = document.getElementById('searchField').value;
+      //const data = Array.from(document.querySelectorAll('.container tr'));
+      let rows = document.querySelectorAll('tbody tr');
+
+
+      for (const row of rows) {
+         row.classList.remove('select');
+         if(inputText !== '' && row.innerHTML.includes(inputText)){
+            row.className = 'select'
+         }
+         
+      }
+     
 
    }
 }
