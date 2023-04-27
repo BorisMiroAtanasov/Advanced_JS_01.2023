a6ca2fb38c0b58935f05b9dc4345530b561bfa58
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-04-27 23:42:42 +0300

    Update solution.js
    task 6 - done

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,48 @@
 function solve() {
-  //TODO
+  let textaArea = document.getElementById('input')
+
+ let textArr = textaArea.value.split('.').filter(s => s !== '');
+
+ let resultDiv = document.getElementById('output');
+
+ while (textArr.length > 0) {
+  let currentText = textArr.splice(0,3).join('. ')+'.';
+  let paragraph = document.createElement('p');
+  paragraph.textContent = currentText;
+
+  resultDiv.appendChild(paragraph);
+
+
+  
+ }
+
+ 
+ 
 }
+
+
+
+
+
+
+// Second solution 
+/*
+function solve() {
+
+  let input = document.getElementById("input").value;
+  // Взимаме инпута и неговата стойност като променлива 
+  let output = document.getElementById("output");
+  // Взимаме outputa като променлива по id 
+
+  let sentences = input.split(".").filter((s) => s.length !== 0);
+  // Изваждаме изреченията от инпута като ги делим по точка и ги филтрираме, за да нямаме празни изречения 
+
+  while (sentences.length > 0) {                                  // Докато има изреченията въртим цикъл 
+    let textParagraph = sentences.splice(0, 3).join(". ") + ".";  
+    // Нашият текст в параграфа ще е максимум 3 изречения като ги съединяваме по точка и интервал и добавяме точка накрая 
+    let p = document.createElement("p");    // създаваме си параграф с createElement и посочваме какъв вид да е в скобите 
+    p.textContent = textParagraph;          // Съдържанието на параграфа ще е това, което сме получили от текста 
+    output.appendChild(p);                  // на outputa създаваме дете с получения параграф 
+  }
+}
+*/
