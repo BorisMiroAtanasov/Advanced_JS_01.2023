92c783b57eb0b309f7eb7849cc78a26f99e09717
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-03 23:33:02 +0300

    Update report.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -4,5 +4,37 @@ function generateReport() {
     const allTr = Array.from(document.querySelectorAll('tbody tr'));
     const result = [];
 
-    debugger
+  
 }
+
+
+
+
+
+
+
+/*
+Todor Qdkov 
+
+function generateReport() {
+    const output = document.querySelector('#output')
+    const info = document.querySelectorAll('tbody tr')
+    const checkboxCheck = document.querySelectorAll('thead tr th input');
+    let result = [];
+
+    output.value = '';
+
+    for (let i = 0; i < info.length; i++) {
+        let newObject = {};
+        for (let j = 0; j < checkboxCheck.length; j++) {
+            if (checkboxCheck[j].checked) {
+                newObject[checkboxCheck[j].name] = info[i].children[j].textContent;
+            }
+        }
+        result.push(newObject);
+    }
+
+    output.value = JSON.stringify(result);
+}
+
+*/
