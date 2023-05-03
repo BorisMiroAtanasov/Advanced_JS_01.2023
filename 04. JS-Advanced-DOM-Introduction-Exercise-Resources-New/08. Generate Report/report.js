1a89118baa39b417497feabf5d5d81d881518ecf
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-03 23:12:06 +0300

    Update report.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,8 @@
 function generateReport() {
+    let checkedList = Array.from(document.querySelectorAll('thead tr th input'));
+    const outPutElement = document.getElementById('output');
+    const allTr = Array.from(document.querySelectorAll('tbody tr'));
+    const result = [];
+
     
 }
