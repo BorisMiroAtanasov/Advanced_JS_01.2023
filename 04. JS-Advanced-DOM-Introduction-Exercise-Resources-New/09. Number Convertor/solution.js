c832b61245d44a3194bae928abdfdc7b4ab983d0
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-04 23:18:24 +0300

    task 9
    task 9 Done

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,13 +1,39 @@
 function solve() {
-    const input = document.getElementById('input').value;
-    const result = document.getElementById('result').value;
+    const input = document.getElementById('input');
+    let result = document.getElementById('result');
 
     const firstOption = document.createElement('option');
     const secondOption = document.createElement('option');
 
-    firstOption.value = 'Binary';
-    secondOption.value = 'Hexadecimal';
-    
+    firstOption.value = 'binary';
+    secondOption.value = 'hexadecimal';
+
+    firstOption.textContent = 'Binary'
+    secondOption.innerHTML = 'Hexadecimal'
+
+    const menuToElement = document.getElementById('selectMenuTo');
+    menuToElement.appendChild(firstOption);
+    menuToElement.appendChild(secondOption);
+
+    const btn = document.getElementsByTagName('button')[0];
+    //console.log('btn clicked');
+    btn.addEventListener('click', () =>{
+        if(menuToElement.value === 'binary') {
+            result.value = Number(input.value).toString(2)
+        }else if (menuToElement.value === 'hexadecimal'){
+            result.value = Number(input.value).toString(16).toUpperCase();
+
+
+        }
+
+    });
+
+   
+
+
+
+
+
     
 
 
