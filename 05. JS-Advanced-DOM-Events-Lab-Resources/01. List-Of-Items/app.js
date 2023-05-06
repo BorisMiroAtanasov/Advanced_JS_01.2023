d940c8c9cf6b47f0fcd691edf323d38f6794b4ed
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-06 12:53:17 +0300

    task 1 - done

--- 05. JS-Advanced-DOM-Events-Lab-Resources/01. List-Of-Items/app.js
+++ 05. JS-Advanced-DOM-Events-Lab-Resources/01. List-Of-Items/app.js
@@ -1,3 +1,15 @@
 function addItem() {
-   
+   const newText = document.getElementById('newItemText').value;
+
+   const textRows = document.getElementById('items');
+
+   let li = document.createElement("li");
+   li.textContent = newText
+
+   textRows.appendChild(li);
+
+   document.getElementById('newItemText').value = ''
+
 }
+
+//document.createElement()
