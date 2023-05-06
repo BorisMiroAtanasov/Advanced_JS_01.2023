5f914494955ba1b1d8e4b0d1c5036c9dc8096e46
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-06 13:32:21 +0300

    task 2
    solution from presentation

--- 05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
+++ 05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
@@ -1,3 +1,45 @@
-function deleteByEmail() {
-    console.log('TODO:...');
+function deleteByEmail(){
+    
 }
+
+
+
+
+
+
+/* from prezentation
+
+function deleteByEmail() {
+    let input = document.querySelector("input[name ='email']").value;
+   //let email = document.getElementsByName("email")[0].value
+
+ 
+
+    const tableRows= Array.from(document.querySelectorAll('tbody tr td'))
+   
+
+    for (const td of tableRows) {
+        if( td.textContent === input) {
+          //  let match = td.textContent;
+            let rowForDel = td.parentNode;
+            rowForDel.parentNode.removeChild(rowForDel);
+            document.getElementById('result').textContent = 'Delete'
+            return;
+            
+        }
+        document.getElementById('result').textContent = 'Not found.'
+
+        
+    }
+
+}
+
+
+/*const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element > 10);
+
+console.log(found);
+// Expected output: 12
+
+*/
