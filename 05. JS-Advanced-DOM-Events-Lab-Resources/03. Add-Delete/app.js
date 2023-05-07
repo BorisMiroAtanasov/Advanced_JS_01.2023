cd36c6711d0d47856530e1f4534199bcf08a1afa
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-07 22:52:35 +0300

    task 3
    Task 3 done

--- 05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
+++ 05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    const newText = document.getElementById('newItemText').value;
+
+    const textRows = document.getElementById('items');
+
+    let li = document.createElement("li");
+    li.textContent = newText;
+
+    const btnDelete = document.createElement('a');
+    btnDelete.href = '#';
+    btnDelete.textContent = '[Delete]';
+    btnDelete.addEventListener('click', () => li.remove());
+    li.appendChild(btnDelete);
+
+
+
+
+    textRows.appendChild(li);
+
+    document.getElementById('newItemText').value = ''
 }
