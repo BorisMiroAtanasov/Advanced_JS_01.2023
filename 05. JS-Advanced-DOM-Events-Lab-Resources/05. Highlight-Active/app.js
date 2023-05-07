1b24b1dd131b5b37417cc967d007f7f08107157b
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-07 23:39:32 +0300

    task 5 Advanced Dom
    task 5 done 1 variant

--- 05. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
+++ 05. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
+    const inputs = Array.from(document.querySelectorAll('input'));
+
+    for (const input of inputs) {
+
+        input.addEventListener('focus' , () =>{
+            input.parentElement.className = 'focused';
+
+        });
+        input.addEventListener('blur' , () =>{
+            input.parentElement.className = '';
+            
+        })
+        
+    }
+
+    
 }
