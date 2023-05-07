5b3c9b95513991645d1f907b7779487eef058996
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-07 23:15:51 +0300

    task 4 DOM Advanced - lab
    task 4 - done

--- 05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
+++ 05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradiaent = document.getElementById('gradient');
+    gradiaent.addEventListener('click', onClick);
+
+
+    function onClick(event){
+        const x = event.offsetX;
+        const percent = Math.floor(x /300 * 100);
+
+        document.getElementById('result').textContent = percent + '%';
+
+        
+
+    }
 }
