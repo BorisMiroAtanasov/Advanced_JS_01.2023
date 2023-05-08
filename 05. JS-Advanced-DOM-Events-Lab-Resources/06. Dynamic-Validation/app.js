f5d0bea8ea5a69dda205a12f393c7e54a3efa7c7
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-09 00:11:47 +0300

    task 6 Dom Advanced
    task 6 - done

--- 05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
+++ 05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,36 @@
 function validate() {
-    console.log('TODO:...');
+    document.getElementById('email').addEventListener('change', onChange);
+
+    function onChange(ev){
+        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if(pattern.test(ev.target.value)){
+            ev.target.classList.remove('error');
+
+        }else{
+            ev.target.classList.add('error');
+
+        }
+
+    }
 }
+
+/*
+function validate() {
+    document.getElementById('email').addEventListener('change', onChange);
+
+    function onChange({target}){
+        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if(pattern.test(target.value)){
+            target.classList.remove('error');
+
+        }else{
+            target.classList.add('error');
+
+        }
+
+    }
+}
+
+*/
