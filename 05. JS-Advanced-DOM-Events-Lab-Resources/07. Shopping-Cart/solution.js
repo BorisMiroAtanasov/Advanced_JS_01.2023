2272dbf01ade5a97b3b0a1cdcd1064ae9560f9c0
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-12 00:36:12 +0300

    task 7  Dom events
    shopping cart - 87%

--- 05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ 05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,4 +1,37 @@
 function solve() {
-let products =(document.getElementsByClassName('product')) ;
-console.log(products);
+document.getElementsByClassName('shopping-cart')[0].addEventListener('click', onClick);
+document.getElementsByClassName('checkout')[0].addEventListener('click',checkOut );
+const cart = [];
+let outPut = document.getElementsByTagName('textarea')[0];
+outPut.value = ''
+
+function onClick(event){
+   if(event.target.tagName =='BUTTON' && event.target.classList.contains('add-product')){
+      const  product = event.target.parentNode.parentNode;
+
+      //console.log(product);
+
+      const productName = product.querySelector('.product-title').textContent;
+      const productPrice = Number(product.querySelector('.product-line-price').textContent);
+      
+      cart.push({
+         productName,
+         productPrice,
+      });
+      outPut.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`
+
+   }
+};
+
+function checkOut(){
+   const products= new Set();
+   cart.forEach(p => products.add(p.productName));
+
+   const total = cart.reduce((t, c) =>t +c.productPrice ,0 );
+   outPut.value += `You bought ${[...products.keys()].join(', ')} for ${total.toFixed(2)}.`
+}
+
+
+
+
 }
