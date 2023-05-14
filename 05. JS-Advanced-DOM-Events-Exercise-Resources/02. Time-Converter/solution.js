0c9e783a8b94133d8b27d6d576550997846679ab
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-14 09:31:34 +0300

    task 2 Dom Events - Time Converter
    task 2 Done

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
@@ -1,4 +1,53 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let inputDays = document.getElementById('days');
+    let inputHours = document.getElementById('hours');
+    let inputMinutes = document.getElementById('minutes');
+    let inputSeconds = document.getElementById('seconds');
+
+    const daysBtn = document.getElementById('daysBtn');
+    const hoursBtn = document.getElementById('hoursBtn');
+    const minutesBtn = document.getElementById('minutesBtn');
+    const secondsBtn = document.getElementById('secondsBtn');
+
+    daysBtn.addEventListener('click',calulateDays );
+    function calulateDays(event){
+        //console.log(event.target);
+        inputHours.value = Number(inputDays.value *24);
+        inputMinutes.value = Number(inputDays.value * 24*60);
+        inputSeconds.value = Number(inputDays.value *24*60*60);
+
+    }
+    hoursBtn.addEventListener('click',calulateHours );
+    function calulateHours(){
+        inputDays.value = Number(inputHours.value / 24);
+        inputMinutes.value = Number(inputHours.value *60);
+        inputSeconds.value = Number(inputHours.value *60*60);
+
+    }
+
+    minutesBtn.addEventListener('click',calulateMinutes );
+    function calulateMinutes(){
+        inputDays.value = Number(inputMinutes.value / 24 / 60);
+        inputHours.value = Number(inputMinutes.value /60);
+        inputSeconds.value = Number(inputMinutes.value *60);
+
+    }
+
+    secondsBtn.addEventListener('click',calulateSeconds );
+    function calulateSeconds(){
+        inputDays.value = Number(inputSeconds.value / 24 / 60 / 60);
+        inputHours.value = Number(inputSeconds.value /60 /60);
+        inputMinutes.value = Number(inputSeconds.value /60);
+
+    }
+
+   
+    inputDays.value = '';
+    inputHours.value = '';
+    inputMinutes.value = '';
+    inputSeconds.value = '';
+
+
+
 }
