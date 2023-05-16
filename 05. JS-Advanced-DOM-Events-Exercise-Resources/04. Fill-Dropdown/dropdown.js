ed7e35247bd217ad7f26dc29d9a6653ae8a647f3
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-16 23:32:09 +0300

    task 4 Dom events exercises
    task 4 Done 100%

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,41 @@
 function addItem() {
-    console.log('TODO:...');
+  let newItemText = document.getElementById("newItemText").value;
+  let newItemValue = document.getElementById("newItemValue").value;
+
+  let option = document.createElement("option");
+  option.text = newItemText;
+  option.value = newItemValue;
+
+  let menu = document.getElementById("menu");
+
+  if(newItemText !== '' && newItemValue !== ''){
+    menu.appendChild(option);
+  }
+  
+
+ document.getElementById("newItemText").value = '';
+  document.getElementById("newItemValue").value = '';
+
+
+
 }
+
+
+// function addItem() {
+//   let newItemText = document.getElementById("newItemText").value;
+//   let newItemValue = document.getElementById("newItemValue").value;
+
+//   let newElement = document.createElement("option");
+
+//   const menu = document.getElementById("menu");
+  
+
+   
+
+//     newElement.textContent = `${newItemText}`;
+//          menu.appendChild(newElement);
+
+  
+
+//   // console.log(newItemText.value + ' ' + newItemValue.value );
+// }
