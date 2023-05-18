96ef104798bb1123bc57b338a1e3b101358fed4a
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-19 00:06:39 +0300

    Update solution.js

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
@@ -1,4 +1,11 @@
 function solve() {
+  const textAreas = document.querySelectorAll('textarea');
+  let inputArea = textAreas[0].value;
+  let outPutArea = textAreas[1].value;
 
-  //TODO...
+  console.log(inputArea);
+  console.log(outPutArea);
+
+
+ 
 }
