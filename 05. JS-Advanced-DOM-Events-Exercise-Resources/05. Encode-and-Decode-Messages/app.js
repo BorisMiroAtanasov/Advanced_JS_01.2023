4be13cb3cca4c2fbf21a5826ceed161705070842
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-18 23:17:19 +0300

    task 5  - Encode and Decode massages - Dom events exercises
    task 5  - done 100%

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,35 +1,59 @@
 function encodeAndDecodeMessages() {
-   let encodeTextArea = Array.from(document.querySelectorAll('textarea'))[0]
-   let dencodeTextArea = Array.from(document.querySelectorAll('textarea'))[1]
+  let encodeTextArea = Array.from(document.querySelectorAll("textarea"))[0];
+  let dencodeTextArea = Array.from(document.querySelectorAll("textarea"))[1];
+  const encodeBtn = Array.from(document.querySelectorAll("button"))[0];
+  const decodeBtn = Array.from(document.querySelectorAll("button"))[1];
+
+  encodeBtn.addEventListener("click", encode);
+  decodeBtn.addEventListener("click", decode);
+
+  function encode() {
+    let stringForEncode = encodeTextArea.value;
+    arrayForEncode = stringForEncode.split("");
+    let result = "";
+    let newStringArr = [];
+
+    for (let i = 0; i < arrayForEncode.length; i++) {
+      let curent = stringForEncode.charCodeAt(i);
+      result = curent + 1;
+      newStringArr.push(result);
+    }
+    let decodeMasage = "";
+    for (let i = 0; i < newStringArr.length; i++) {
+      let currentLetter = String.fromCharCode(newStringArr[i]);
+
+      decodeMasage += currentLetter;
+    }
+
+    encodeTextArea.value = "";
+    dencodeTextArea.value = decodeMasage;
+  }
+
+  function decode(){
+    let stringForDecode = dencodeTextArea.value;
+    arrayForDecode = stringForDecode.split("");
+    let result = "";
+    let newStringArr = [];
+
+    for (let i = 0; i < arrayForDecode.length; i++) {
+      let curent = stringForDecode.charCodeAt(i);
+      result = curent - 1;
+      newStringArr.push(result);
+    }
+    let decodeMasageNew = "";
+    for (let i = 0; i < newStringArr.length; i++) {
+      let currentLetter = String.fromCharCode(newStringArr[i]);
+
+      decodeMasageNew += currentLetter;
+    }
+
+    encodeTextArea.value = "";
+    dencodeTextArea.value = decodeMasageNew;
+
+  }
 
 
-   let stringForEncode = encodeTextArea.value;
-   arrayForEncode = stringForEncode.split('');
-   let result =''
-   let newStringArr = []
 
-
-  // console.log(arrayForEncode);
-
-   for (let i=0 ; i < arrayForEncode.length ; i++){
-    let curent = stringForEncode.charCodeAt(i);
-    result = curent + 1;
-    newStringArr.push(result);
-
-
-   }
-   for(let i = 0 ; i<newStringArr.length ; i++ ){
-    let currentLetter = newStringArr.fromCharCode(i)
-    console.log(currentLetter);
-   }
-
-
-//    console.log(newStringArr);
-//    let res = String.fromCharCode(newStringArr);
-//    console.log(res);
-   
-
-   
 
 
 }
