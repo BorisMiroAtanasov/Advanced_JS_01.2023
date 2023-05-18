0a681c0f4fdb9832b83e5ad09141a0242f4c241d
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-18 22:43:52 +0300

    Update app.js

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -6,21 +6,28 @@ function encodeAndDecodeMessages() {
    let stringForEncode = encodeTextArea.value;
    arrayForEncode = stringForEncode.split('');
    let result =''
+   let newStringArr = []
 
 
   // console.log(arrayForEncode);
 
    for (let i=0 ; i < arrayForEncode.length ; i++){
     let curent = stringForEncode.charCodeAt(i);
-    result = curent + 1 
+    result = curent + 1;
+    newStringArr.push(result);
 
 
-
-
-
-
-    console.log(result);
    }
+   for(let i = 0 ; i<newStringArr.length ; i++ ){
+    let currentLetter = newStringArr.fromCharCode(i)
+    console.log(currentLetter);
+   }
+
+
+//    console.log(newStringArr);
+//    let res = String.fromCharCode(newStringArr);
+//    console.log(res);
+   
 
    
 
