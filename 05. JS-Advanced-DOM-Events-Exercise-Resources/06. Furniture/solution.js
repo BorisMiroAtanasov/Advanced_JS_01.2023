d39c5e3d28095c330a28f6fc4b45c16f22627665
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-19 20:19:49 +0300

    Update solution.js

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
@@ -1,7 +1,32 @@
 function solve() {
   const [generateBtn, buyBtn] = Array.from(document.querySelectorAll('button'));
-  console.log(generateBtn);
-  console.log(buyBtn);
+  
+
+  generateBtn.addEventListener('click',generate);
+ // buyBtn.addEventListener('click',buy);
+
+ function generate(){
+  const input = JSON.parse(document.querySelector('textarea').value)
+  input.forEach(element => {
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const img = document.createElement('img');
+    img.src = element.img;
+    td1.appendChild(img);
+    tr.appendChild(td1);
+
+    const td2 = document.createElement('td');
+    const p1 = document.createElement('p');
+    p1.textContent = element.name;
+    td2.appendChild(p1);
+    tr.appendChild(td2)
+
+
+
+
+    
+  });
+ }
 
 
   
