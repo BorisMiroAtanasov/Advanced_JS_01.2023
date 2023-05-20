aa2bced2be338af00861c548c11b9760acbb02a9
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-20 16:43:26 +0300

    Update distanceConverter.js

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/07. Distance-Converter/distanceConverter.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,35 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const inputDistance = document.getElementById('inputDistance');
+    const outputDistance = document.getElementById('outputDistance');
+    const inputUnits = document.getElementById('inputUnits');
+    const outputUnits = document.getElementById('outputUnits');
+   
+    const convertBtn = document.getElementById('convert');
+
+
+    convertBtn.addEventListener('click', convert);
+    const data = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        m1: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254,
+    }
+
+    function convert(){
+      if (inputUnits.value == 'm'){
+        console.log(outputDistance.value = 1);
+      }
+    };
+
+    
+
+    
+
+    
+
+
 }
