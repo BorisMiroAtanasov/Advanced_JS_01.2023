0bf41cb8cf12e52af3247d60226475421aeb30c5
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-20 19:31:31 +0300

    Lab Advanced Function
    task 1 - Area and Volume Calculator   Viktor solution

--- 13 -  JS-Advanced-Advanced-Functions - Lab/01_Area_and_volume_calculator_Viktor.js
+++ 13 -  JS-Advanced-Advanced-Functions - Lab/01_Area_and_volume_calculator_Viktor.js
@@ -1,3 +1,21 @@
+function areaVol(areaFunc, volFunc, sahpesJson) {
+  const shapes = JSON.parse(sahpesJson);
+
+  const result = [];
+  for (let shape of shapes) {
+    const area = areaFunc.call(shape);
+    const volume = volFunc.call(shape);
+
+    const current = {
+      area,
+      volume,
+    };
+    result.push(current)
+  }
+
+ return result;
+}
+
 function area() {
   return Math.abs(this.x * this.y);
 }
@@ -6,16 +24,19 @@ function vol() {
   return Math.abs(this.x * this.y * this.z);
 }
 
-function solve(area, vol, input) {
+const s1 = `[
+  {"x":"1","y":"2","z":"10"},
+  {"x":"7","y":"7","z":"10"},
+  {"x":"5","y":"2","z":"10"}
+  ]`;
 
-   
-}
+const s2 = `[
+    {"x":"10","y":"-22","z":"10"},
+    {"x":"47","y":"7","z":"-5"},
+    {"x":"55","y":"8","z":"0"},
+    {"x":"100","y":"100","z":"100"},
+    {"x":"55","y":"80","z":"250"}
+    ]`;
 
-
-console.log(solve(area, vol, `[
-    {"x":"1","y":"2","z":"10"},
-    {"x":"7","y":"7","z":"10"},
-    {"x":"5","y":"2","z":"10"}
-    ]`))
-
-
+areaVol(area, vol, s1);
+areaVol(area, vol, s2);
