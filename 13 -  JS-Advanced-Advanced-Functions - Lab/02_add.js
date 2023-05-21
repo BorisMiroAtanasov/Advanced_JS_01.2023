d8f407e3877e4fc7b4f913cad318b234a4fb0a10
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-21 12:54:14 +0300

    Create 02_add.js

---
+++ 13 -  JS-Advanced-Advanced-Functions - Lab/02_add.js
@@ -0,0 +1,18 @@
+function solution(num) {
+  function add(n) {
+   n += num;
+    return n
+  }
+  return add
+
+ 
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
