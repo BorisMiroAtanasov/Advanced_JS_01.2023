94277db3376253c6cd598df65c9ebcc73bf01e4c
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-22 21:37:29 +0300

    task 4 Advanced function
    task 4 not done

---
+++ 13 -  JS-Advanced-Advanced-Functions - Lab/04_filter_emploees.js
@@ -0,0 +1,31 @@
+function filterEmploees(data, criteria){
+    let dataArr = JSON.parse(data);
+    console.log(dataArr);
+    console.log(criteria);
+    
+
+}
+
+
+
+filterEmploees(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`, 
+'gender-Female')
