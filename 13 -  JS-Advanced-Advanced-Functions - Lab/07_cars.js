dce9f1b077da64fb16a6520438ade9f38531d909
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-23 23:24:11 +0300

    Create 07_cars.js

---
+++ 13 -  JS-Advanced-Advanced-Functions - Lab/07_cars.js
@@ -0,0 +1,32 @@
+function cars(inputArr){
+    let outPutArr = [];
+
+    for (const el of inputArr) {
+        let [command, name, action , property] = el.split(' ');
+
+        
+        
+
+    }
+
+    function create(){
+
+    }
+    function inherit(){
+
+    }
+
+    function print(){
+        
+    }
+
+   
+
+}
+
+cars(['create c1',
+'create c2 inherit c1',
+'set c1 color red',
+'set c2 model new',
+'print c1',
+'print c2'])
