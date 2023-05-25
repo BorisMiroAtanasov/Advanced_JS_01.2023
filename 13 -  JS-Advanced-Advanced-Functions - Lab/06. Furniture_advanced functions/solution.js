e262a39b78e5f74718d2b2df458483929d8b5cb6
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-26 00:25:42 +0300

    Furniture - closur
    furniture

--- 13 -  JS-Advanced-Advanced-Functions - Lab/06. Furniture_advanced functions/solution.js
+++ 13 -  JS-Advanced-Advanced-Functions - Lab/06. Furniture_advanced functions/solution.js
@@ -1,61 +1,80 @@
 function solve() {
-  const [input, outupt] = Array.from(document.querySelectorAll("textarea"));
+  const [input, output] = Array.from(document.querySelectorAll("textarea"));
   const [generateBtn, buyBtn] = Array.from(document.querySelectorAll("button"));
   const tbody = document.querySelector("tbody");
 
   generateBtn.addEventListener("click", generate);
+  buyBtn.addEventListener("click", buy);
+
+  const items = [];
 
   function generate() {
     const data = JSON.parse(input.value);
 
     for (const item of data) {
       const row = createRow(item);
-      tbody.appendChild(row);
+      tbody.appendChild(row.element);
+      items.push(row);
     }
   }
+
+  function buy() {
+    const selectItems = items.filter(i => i.isChecked()).map(e => e.item);
+    const names = selectItems.map(i => i.name).join(', ');
+    const total = selectItems.reduce((a,c) => a + Number(c.price), 0);
+    const deckFactor  = selectItems.reduce((a,c,i,arr) => a + Number(c.decFactor)/arr.length, 0);
+
+    output.textContent = [
+      `Bought furniture: ${names}`,
+      `Total price: ${total.toFixed(2)}`,
+      `Average decoration factor: ${deckFactor}`
+    ].join('\n');
+  }
+
   function createRow(item) {
     const row = document.createElement("tr");
-
-    
-    
-    
 
     row.appendChild(createCol(createImg(item.img)));
     row.appendChild(createCol(createPara(item.name)));
     row.appendChild(createCol(createPara(item.price)));
     row.appendChild(createCol(createPara(item.decFactor)));
-    row.appendChild(createCol(createCheck()));
+    const check = createCheck();
+    row.appendChild(createCol(check));
 
-    return row;
+    return {
+      element: row,
+      item,
+      isChecked,
+    };
+    function isChecked(){
+      return check.checked
+    }
   }
 
   function createCol(content) {
     const col = document.createElement("td");
     col.scope = "col";
-    
+
     col.appendChild(content);
     return col;
   }
 
-  function createPara(content){
+  function createPara(content) {
     const p = document.createElement("p");
     p.textContent = content;
 
     return p;
-
   }
 
-  function createImg(src){
+  function createImg(src) {
     const img = document.createElement("img");
     img.src = src;
     return img;
-
   }
 
-  function createCheck(){
+  function createCheck() {
     const check = document.createElement("input");
-    check.type = 'checkbox';
+    check.type = "checkbox";
     return check;
-
   }
 }
