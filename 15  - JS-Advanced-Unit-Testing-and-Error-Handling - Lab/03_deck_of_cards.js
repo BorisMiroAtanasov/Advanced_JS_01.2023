d41e24589004b67bfc8e4ef9ec9b92eba338c71e
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-27 00:08:01 +0300

    Unit testing
    deck of cards - second solution

--- 15  - JS-Advanced-Unit-Testing-and-Error-Handling - Lab/03_deck_of_cards.js
+++ 15  - JS-Advanced-Unit-Testing-and-Error-Handling - Lab/03_deck_of_cards.js
@@ -1,54 +1,60 @@
 function printDeckOfCards(cards) {
-    // let eachCard = cards.split(', ');
-    // console.log(eachCard);
-    for (const eachCard of cards) {
-        let [face, suit] = eachCard.split('');
-        console.log(face);
-    }
+  let result = [];
 
-    function createCard (face, suit){
-        let faces = [
-            '2',
-            '3',
-            '4',
-            '5',
-            '6',
-            '7',
-            '8',
-            '9',
-            '10',
-            'J',
-            'Q',
-            'K',
-            'A',
-        ]
-        let suits = {
-            'S':'\u2660',
-            'H':'\u2665',
-            'D':'\u2666',
-            'C':'\u2663',
-        }
-    
-        if(faces.includes(face) == false){
-            throw new TypeError('Invalid face');
-    
-        } if(suit in suits == false){
-            throw new TypeError('Invalid suit');
-        }
-    
-        const result ={
-            face,
-            suit,
-            toString(){
-                return this.face + suits[this.suit]
-            }
-        };
-        return result
+  for (const eachCard of cards) {
+    const face = eachCard.slice(0, -1);
+    const suit = eachCard.slice(-1);
+    let newCard = "";
+
+    try {
+      newCard = createCard(face, suit);
+      result.push(newCard);
+    } catch (err) {
+        result = [`Invalid card: ${eachCard}`]
     }
-      
   }
+  console.log(result.join(' '));
 
-  printDeckOfCards(['AS', '10D', 'KH', '2C']);
-  printDeckOfCards(['5S', '3D', 'QD', '1C']);
+  function createCard(face, suit) {
+    let faces = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A",
+    ];
+    let suits = {
+      S: "\u2660",
+      H: "\u2665",
+      D: "\u2666",
+      C: "\u2663",
+    };
 
+    if (faces.includes(face) == false) {
+      throw new TypeError("Invalid face");
+    }
+    if (suit in suits == false) {
+      throw new TypeError("Invalid suit");
+    }
 
+    const result = {
+      face,
+      suit,
+      toString() {
+        return this.face + suits[this.suit];
+      },
+    };
+    return result;
+  }
+}
+
+printDeckOfCards(["AS", "10D", "KH", "2C"]);
+printDeckOfCards(["5S", "3D", "QD", "1C"]);
