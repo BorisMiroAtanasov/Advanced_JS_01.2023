2c6aedec2f5c3a10887c26d141e238c1b45d6318
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-26 22:58:29 +0300

    Unit testing
    task 2 cards - done

---
+++ 15  - JS-Advanced-Unit-Testing-and-Error-Handling - Lab/03_deck_of_cards.js
@@ -0,0 +1,54 @@
+function printDeckOfCards(cards) {
+    // let eachCard = cards.split(', ');
+    // console.log(eachCard);
+    for (const eachCard of cards) {
+        let [face, suit] = eachCard.split('');
+        console.log(face);
+    }
+
+    function createCard (face, suit){
+        let faces = [
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '10',
+            'J',
+            'Q',
+            'K',
+            'A',
+        ]
+        let suits = {
+            'S':'\u2660',
+            'H':'\u2665',
+            'D':'\u2666',
+            'C':'\u2663',
+        }
+    
+        if(faces.includes(face) == false){
+            throw new TypeError('Invalid face');
+    
+        } if(suit in suits == false){
+            throw new TypeError('Invalid suit');
+        }
+    
+        const result ={
+            face,
+            suit,
+            toString(){
+                return this.face + suits[this.suit]
+            }
+        };
+        return result
+    }
+      
+  }
+
+  printDeckOfCards(['AS', '10D', 'KH', '2C']);
+  printDeckOfCards(['5S', '3D', 'QD', '1C']);
+
+
