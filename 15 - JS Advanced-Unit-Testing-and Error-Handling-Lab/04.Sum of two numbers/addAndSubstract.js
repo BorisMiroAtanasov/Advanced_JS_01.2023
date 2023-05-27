3177ba432af342f4136125a58bacdfdc2db272bf
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-27 16:12:39 +0300

    Unit testing
    task 7 add and subtract  - don't pass in Juge

--- 15 - JS Advanced-Unit-Testing-and Error-Handling-Lab/04.Sum of two numbers/addAndSubstract.js
+++ 15 - JS Advanced-Unit-Testing-and Error-Handling-Lab/04.Sum of two numbers/addAndSubstract.js
@@ -1,6 +1,43 @@
+
+function createCalculator() {
+    let value = 0;
+    return {
+        add: function(num) { value += Number(num); },
+        subtract: function(num) { value -= Number(num); },
+        get: function() { return value; }
+    }
+}
+
+module.exports = createCalculator;
+
+
+
+
+
+
+/* Виктор Костадинов - функция за да минават тестовете , без да имаме вярното(предварително) решение 
+
+
 function createCalkulator(){
+    let sum = 0;
+
+    return {
+        add(value){
+            sum += Number(value)
+
+        },
+        subtract(value){
+            sum -= value
+        },
+        get(){
+            return sum
+        }
+
+    }
 
 }
 
 
 module.exports = createCalkulator;
+
+*/
--- 15 - JS Advanced-Unit-Testing-and Error-Handling-Lab/04.Sum of two numbers/addAndSubstract.test.js
+++ 15 - JS Advanced-Unit-Testing-and Error-Handling-Lab/04.Sum of two numbers/addAndSubstract.test.js
@@ -1,3 +1,80 @@
-const {expect} = require('chai');
-const createCalkulator = require('./addAndSubstract');
+const { expect } = require("chai");
+const createCalkulator = require("./addAndSubstract");
 
+describe("Test calculator", function () {
+  let calc = null;
+  beforeEach(() => {
+    calc = createCalkulator();
+  });
+  it("returns zero", () => {
+    expect(calc.get()).to.equal(0);
+  });
+  it("has correct structure", () => {          // можем да проверим типа но по принцип не трябва да се прави - Виктор Костадинов
+
+    expect(typeof calc).to.equal('object');
+    expect(calc).to.has.ownProperty('add');
+    expect(calc).to.has.ownProperty('subtract');
+    expect(calc).to.has.ownProperty('get');
+  });
+
+
+  describe('Number arguments', ()=>{
+   
+      it("can add number", () => {
+        calc.add(1);
+        expect(calc.get()).to.equal(1);
+      });
+      it("can add number", () => {
+        calc.add(1);
+        calc.add(1);
+        expect(calc.get()).to.equal(2);
+      });
+      it("can subtract number", () => {
+        calc.subtract(1);
+        expect(calc.get()).to.equal(-1);
+      });
+      it("can add and subtract number", () => {
+        calc.add(2);
+        calc.subtract(1);
+        expect(calc.get()).to.equal(1);
+      });
+
+  });
+
+  describe('string arguments', ()=>{
+    it('can add numbers as strings', ()=>{
+        calc.add('1');
+        expect(calc.get()).to.equal(1);
+    });
+    it('can subtracts numbers as strings', ()=>{
+        calc.subtract('1');
+        expect(calc.get()).to.equal(-1);
+    });
+  });
+  describe('Test overLoading', ()=>{
+    it('overloading 1', ()=>{
+        calc.add('2');
+        expect(calc.get()).to.equal(2);
+    });
+    it('overloading 2', ()=>{
+        calc.add('2');
+        calc.add('2');
+        expect(calc.get()).to.equal(4);
+    });
+    it('overloading 3', ()=>{
+        calc.add('1');
+        calc.add('2');
+        calc.add('3');
+        expect(calc.get()).to.equal(6);
+    });
+    it('overloading 4', ()=>{
+        calc.add(1);
+        calc.add(2);
+        calc.add(3);
+        calc.subtract(4);
+        expect(calc.get()).to.equal(2);
+    });
+  });
+
+  
+});
