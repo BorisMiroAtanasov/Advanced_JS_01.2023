acfc8de1ca893faa58b7001baf754b3b50e60545
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-29 23:53:32 +0300

    Update 01.request_Validator.js

--- 16 - JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/01.request_Validator.js
+++ 16 - JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/01.request_Validator.js
@@ -1,8 +1,31 @@
-function requestValidator(obj){
+function requestValidator(object){
 
+    let patern = /([a-z.0-9]+)/gm;
+    
+
+    if (object.method !== 'GET' && object.method !== 'POST' && object.method !== 'DELETE' && object.method !== 'CONNECT' ){
+        console.log(`Invalid request header: Invalid Method`);
+    }else if(object.uri !== 1){
+       
+        
+
+    }
+
+/// dasdadadasdasddddgggdddadadaddddd
 
 }
-requestValidator();
 
+   
+requestValidator({
+  method: 'GET',
+  uri: 'svn.public.catalog',
+  version: 'HTTP/1.1',
+  message: ''
+});
 
-requestValidator();
+requestValidator({
+  method: 'OPTIONS',
+  uri: 'git.master',
+  version: 'HTTP/1.1',
+  message: '-recursive'
+});
