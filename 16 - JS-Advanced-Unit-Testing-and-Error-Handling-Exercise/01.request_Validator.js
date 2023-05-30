04067803e52b21814a23d843e0d2859c7fcd0124
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-05-31 00:45:05 +0300

    Unit Testing
    task 1 Request validator - done

--- 16 - JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/01.request_Validator.js
+++ 16 - JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/01.request_Validator.js
@@ -1,30 +1,31 @@
 function requestValidator(object){
 
-    let methodArr = ['GET', 'POST', 'DELETE' , 'CONNECT']
+    let methodArr = ['GET', 'POST', 'DELETE' , 'CONNECT'];
+    let versionArr = ["HTTP/0.9","HTTP/1.0", "HTTP/1.1","HTTP/2.0"]
 
     let patern = /^[\w.]+$/gm          
 
-    let maches = object.uri.match(patern);
+   
     let massageRex = /^[^<>\\&'\"]*$/gm;
 
     
     
 
-    if (!(object.hasOwnProperty('method') &&  (object.method == 'GET' || object.method == 'POST' || object.method == 'DELETE' || object.method == 'CONNECT'))){
+    if (!(object.hasOwnProperty('method') &&  methodArr.includes(object.method))){
         throw new Error(`Invalid request header: Invalid Method`);
         
     }
-    if(!object.hasOwnProperty('uri') || !patern.test(object.uri)){    
-        throw new Error(`Invalid request header: URI`);
+    if(!(object.hasOwnProperty('uri') &&(object.uri === "*" || patern.test(object.uri)))){    
+        throw new Error("Invalid request header: Invalid URI");
        
     }
 
-    if( !object.hasOwnProperty('version') || object.version !=='' && ( object.version !== "HTTP/0.9" && object.version !== "HTTP/1.0" && object.version !== "HTTP/1.1" && object.version !== "HTTP/2.0" )){
-        throw new Error(`Invalid request header: Version`);
+    if( !(object.hasOwnProperty('version') &&  versionArr.includes(object.version))){
+        throw new Error(`Invalid request header: Invalid Version`);
     }
 
-    if(!object.hasOwnProperty('message')  || massageRex.test(object.message)){
-        throw new Error(`Invalid request header: Message`);
+    if(!(object.hasOwnProperty('message')&& ( object.message ==='' || massageRex.test(object.message)))){
+        throw new Error(`Invalid request header: Invalid Message`);
     }
 
    return (object);
