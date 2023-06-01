5f275b67f00f1ceaecf0dd86b41e3f0e8a018369
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-01 23:54:57 +0300

    update char lookup
    done

--- 16.JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/Unit testing/03_.char_lookup.test.js
+++ 16.JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/Unit testing/03_.char_lookup.test.js
@@ -13,9 +13,14 @@ describe('Unit testing Cahr LookUp', function(){
         let index = 0
         expect(lookupChar(string,index)).to.be.undefined;
     });
+    it('Returns undefined - first param is num and second param is a num',()=>{
+        let string = 1;
+        let index = 1
+        expect(lookupChar(string,index)).to.be.undefined;
+    });
     it('Returns Incorect index - first is a string and second param is a float num',()=>{
         let string = 'aaa';
-        let index = 2.2;
+        let index = 1.2;
         expect(lookupChar(string,index)).to.be.undefined;
     });
     it('Returns undefided - first is a string and second string',()=>{
@@ -26,12 +31,12 @@ describe('Unit testing Cahr LookUp', function(){
 
     it('Returns Incorect index - first is a string and second param is not in range',()=>{
         let string = 'aaa';
-        let index = 4;
+        let index = 10;
         expect(lookupChar(string,index)).to.be.equal('Incorrect index');
     });
     it('Returns Incorect index - first is a string and second param is not in range negative num',()=>{
         let string = 'aaa';
-        let index = -1;
+        let index = -10;
         expect(lookupChar(string,index)).to.be.equal('Incorrect index');
     });
     it('Returns Incorect index - first is a string and second param equal to the string length',()=>{
@@ -40,12 +45,6 @@ describe('Unit testing Cahr LookUp', function(){
         expect(lookupChar(string,index)).to.be.equal('Incorrect index');
     });
 
-    it('Returns Incorect index - first is a string and second param is not in range negative num',()=>{
-        let string = 'aaa';
-        let index = -1;
-        expect(lookupChar(string,index)).to.be.equal('Incorrect index');
-    });
-   
 
     it('Returns undefined - first is a empty string and second param is  in range',()=>{
         let string = '';
@@ -63,6 +62,14 @@ describe('Unit testing Cahr LookUp', function(){
         let string = {aaa: 1};
         let index = 1;
         expect(lookupChar(string,index)).to.be.undefined;
+    });
+
+
+    
+    it('Returns correct- first param is string , second param is num in range',()=>{
+        let string = 'Hello'
+        let index = 1;
+        expect(lookupChar(string,index)).to.be.equal('e');
     });
     
 
