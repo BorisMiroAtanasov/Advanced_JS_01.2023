b8c5f868c9a6c92327471986cd576fd70fe026fa
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-02 23:39:28 +0300

    da

--- 16.JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/Unit testing/04_math_enforcer.test.js
+++ 16.JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/Unit testing/04_math_enforcer.test.js
@@ -1,4 +1,11 @@
 const {expect, assert} = require ("chai")
 
-const {lookupChar} = require ("./04_math_enforcer");
+const {mathEnforcer} = require ("./04_math_enforcer");
 
+describe('Test of mathEnforcer', function(){
+    describe('test addFive method', function(){
+        it('returns undefined with non-number input',()=>{
+            expect(mathEnforcer.addFive('1')).to.be.undefined;
+        });
+    });
+});
