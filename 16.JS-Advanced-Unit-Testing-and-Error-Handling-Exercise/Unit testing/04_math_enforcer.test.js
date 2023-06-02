5b2b891dce5cdc4300b7c456c64ab094106cc572
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-02 23:52:47 +0300

    update mathEnforser
    update

--- 16.JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/Unit testing/04_math_enforcer.test.js
+++ 16.JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/Unit testing/04_math_enforcer.test.js
@@ -4,8 +4,24 @@ const {mathEnforcer} = require ("./04_math_enforcer");
 
 describe('Test of mathEnforcer', function(){
     describe('test addFive method', function(){
-        it('returns undefined with non-number input',()=>{
+        it('returns undefined , pram is not a number',()=>{
             expect(mathEnforcer.addFive('1')).to.be.undefined;
         });
+        it('returns correct answer - add five to  the number input',()=>{
+            expect(mathEnforcer.addFive(1)).to.be.equal(6);
+        });
+    });
+    describe('test subtract method', function(){
+        it('returns undefined, the param is not a number',() =>{
+            expect(mathEnforcer.subtractTen('1')).to.be.undefined;
+        });
+        it('returns correct answer, the param is number',() =>{
+            expect(mathEnforcer.subtractTen(1)).to.be.equal(-9);
+        });
+
+    });
+
+    describe('test sum method', function(){
+
     });
 });
