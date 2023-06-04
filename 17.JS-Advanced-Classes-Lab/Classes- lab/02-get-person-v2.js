a2dc613052429d5540c46656d6788d1078b3934f
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-04 23:12:47 +0300

    Classes lab
    task 2 two variants - done

---
+++ 17.JS-Advanced-Classes-Lab/Classes- lab/02-get-person-v2.js
@@ -0,0 +1,58 @@
+function getPerson(){
+    class Person {
+        constructor(firstName, lastName, age, email) {
+          this.firstName = firstName;
+          this.lastName = lastName;
+          this.age = age;
+          this.email = email;
+        }
+      
+        toString() {
+          return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+      }
+
+   let personArray =[];
+   let preson1 = new Person("Anna", "Simpson", 22, "anna@yahoo.com");
+   personArray.push(preson1);
+   let preson2 = new Person("SoftUni");
+   personArray.push(preson2);
+   let preson3 = new Person("Stephan", "Johnson", 25);
+   personArray.push(preson3);
+   let preson4 = new Person("Gabriel", "Peterson", 24, "g.p@gmail.com");
+   personArray.push(preson4);
+   return personArray
+
+
+
+
+}
+
+
+/*
+  function getPersons() {
+
+    class Person {                                          // Създаваме клас с име Person
+        constructor(firstName, lastName, age, email) {      // Създаваме конструктор, който приема 4 параметъра
+            this.firstName = firstName;                     // Създаваме пропърти firstName, което е подаденият ни параметър firstName
+            this.lastName = lastName;                       // Създаваме пропърти lastName, което е подаденият ни параметър lastName
+            this.age = age;                                 // Създаваме пропърти age, което е подаденият ни параметър age
+            this.email = email;                             // Създаваме пропърти email, което е подаденият ни параметър email
+        }
+        toString() {                                        // Създаваме метод към класа
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`; // Той ще връща това съобщение
+        }
+    }
+
+    let persons = [];                                                  // Създаваме си празен масив, за хората които ще създадем 
+    let person1 = new Person("Anna", "Simpson", 22, "anna@yahoo.com");  // Създаваме първия ни човек 
+    persons.push(person1);                                              // Добавяме създадената инстанция към масива             
+    let person2 = new Person("SoftUni");                                // Създаваме втория ни човек
+    persons.push(person2);                                              // Добавяме създадената инстанция към масива 
+    let person3 = new Person("Stephan", "Johnson", 25);                 // Създаваме третия ни човек
+    persons.push(person3);                                              // Добавяме създадената инстанция към масива 
+    let person4 = new Person("Gabriel", "Peterson", 24, "g.p@gmail.com");   // Създаваме четвъртия ни човек
+    persons.push(person4);                                              // Добавяме създадената инстанция към масива 
+    return persons;                                                     // Връщаме масива със създадените обекти от класа Person
+}
+*/
