db0af6c5d17a7e05adc3487447b2d07f8ea6975c
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-04 22:40:33 +0300

    Classes - lab
    task 1 personal info - done

--- 17.JS-Advanced-Classes-Lab/Classes- lab/01-personal-info-v2.js
+++ 17.JS-Advanced-Classes-Lab/Classes- lab/01-personal-info-v2.js
@@ -1,16 +1,15 @@
-class Person{
-    constructor(firstName, lastName, age, email){
-        firstName = this.firstName;
-        lastName = this.lastName;
-        age = this.age;
-        email = this.email;
+class Person {
+  constructor(firstName, lastName, age, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.email = email;
+  }
 
-    }
-
-    
+  toString() {
+    return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+  }
 }
 
-
-
-let person = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
-console.log(person.toString())
+let person = new Person("Anna", "Simpson", 22, "anna@yahoo.com");
+console.log(person.toString());
