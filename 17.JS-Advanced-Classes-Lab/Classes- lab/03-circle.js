fea5117fb0da43b2058037fbb8f3e77b82c8b9b3
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-04 23:36:41 +0300

    Classes lab
    task 3 Circle - done

--- 17.JS-Advanced-Classes-Lab/Classes- lab/03-circle.js
+++ 17.JS-Advanced-Classes-Lab/Classes- lab/03-circle.js
@@ -1,53 +1,25 @@
 class Circle{
     constructor(radius){
         this.radius = radius;
+    }
 
+    get diameter(){
+        return  this.radius * 2
+    }
+
+    set diameter(value){
+        this.radius = value/2
+    }
+
+   
+
+    get area(){
+        return Math.PI * (this.radius)**2
 
     }
 
-    get radius(){
-        return this._radius;
-            
-    }
 
-    set radius(value){
-        if( typeof this.radius != 'number'){
-            throw TypeError('Radius must be a Number')
-        }
-
-        return this._radius = value;
-
-    }
-    get diametar(){
-       return 2 * this.radius;
-
-    }
-
-    set diametar(value){
-        if( typeof this.diametar != 'number'){
-            throw TypeError('Diametar must be a Number')
-        }
-        this.radius = value / 2;
-
-    }
 }
-
-
-
-// const myCircle = new Circle(5)
-// console.log(myCircle.radius);
-// console.log(myCircle.diametar);
-
-
-// myCircle.radius = 8
-
-// console.log(myCircle.radius);
-// console.log(myCircle.diametar);
-
-// myCircle.radius = 20
-
-// console.log(myCircle.radius);
-// console.log(myCircle.diametar);
 
 let c = new Circle(2);
 console.log(`Radius: ${c.radius}`);
@@ -57,4 +29,3 @@ c.diameter = 1.6;
 console.log(`Radius: ${c.radius}`);
 console.log(`Diameter: ${c.diameter}`);
 console.log(`Area: ${c.area}`);
-
