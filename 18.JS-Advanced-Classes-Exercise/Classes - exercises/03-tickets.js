6114cdf49e0ee13f16bf93f6c37f57dabcdc04a6
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-06 23:59:12 +0300

    Classes exercises
    task 3 Tickets - done

--- 18.JS-Advanced-Classes-Exercise/Classes - exercises/03-tickets.js
+++ 18.JS-Advanced-Classes-Exercise/Classes - exercises/03-tickets.js
@@ -1,38 +1,40 @@
-function ticketSort(ticketArr, criteria){
-
-    class Ticket{
-        constructor(destination, price, status){
-            this.destination = destination;
-            this.price = price;
-            this.status = status;
-
-        }
+function ticketSort(ticketArr, criteria) {
+  class Ticket {
+    constructor(destination, price, status) {
+      this.destination = destination;
+      this.price = price;
+      this.status = status;
     }
+  }
 
-    let result = [];
-    for (const element of ticketArr) {
-        let [destination, price, status] = element.split('|');
+  let result = [];
+  for (const element of ticketArr) {
+    let [destination, price, status] = element.split("|");
+    price = Number(price);
 
     result.push(new Ticket(destination, price, status));
-        
-    
+  }
+
+  return result.sort((a, b) => {
+    if ( typeof a[criteria] === "number") {
+      return a[criteria] - b[criteria];
+    } else {
+      return a[criteria].localeCompare(b[criteria]);
     }
+  });
 
-
-    
-            
-        
-    
-
+  //console.log(result);
 }
 
-ticketSort(['Philadelphia|94.20|available',
-'New York City|95.99|available',
-'New York City|95.99|sold',
-'Boston|126.20|departed'],
-'destination');
-
-
+ticketSort(
+  [
+    "Philadelphia|94.20|available",
+    "New York City|95.99|available",
+    "New York City|95.99|sold",
+    "Boston|126.20|departed",
+  ],
+  "destination"
+);
 
 // ticketSort(['Philadelphia|94.20|available',
 // 'New York City|95.99|available',
