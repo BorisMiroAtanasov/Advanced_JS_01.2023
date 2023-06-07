e8b97b7413c35a67d3da9747f10a39fa4b5e1b24
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-08 00:17:00 +0300

    Classes exercises
    task 4 Sorted list - done

--- 18.JS-Advanced-Classes-Exercise/Classes - exercises/04-sorted-list.js
+++ 18.JS-Advanced-Classes-Exercise/Classes - exercises/04-sorted-list.js
@@ -1,37 +1,36 @@
-
-
 class List {
-    
-    constructor(){
-        this.result = [];
-        this.size = this.result.length
+  constructor() {
+    this.result = [];
+    this.size = this.result.length;
+  }
 
+  add(value) {
+    this.result.push(value);
+    this.size = this.result.length;
+    this.result.sort((a, b) => a - b);
+  }
+
+  remove(index) {
+    if (index >= this.result.length || index < 0) {
+      throw new Error(`Invalid index`);
     }
-   
-     add(value){
-        this.result.push(value);
-        this.size = this.result.length;
-        this.result.sort(a,b => a-b)
-                }
-
-     remove(index) {
-        if(index >= this.result.length || index < 0 ){
-            throw Error(`Error`)
-            
-        }
-        this.result.slice(index,1);
-
-     }         
+    this.result.splice(index, 1);
+    this.size = this.result.length;
+  }
+  get(index) {
+    if(index >= this.result.length || index < 0){
+        throw new Error(`Invalid index`);
+    }
+    return this.result[index]
 
 
-
+  }
 }
 
 let list = new List();
 list.add(5);
 list.add(6);
 list.add(7);
-console.log(list); 
-//console.log(list.get(1)); 
-//list.remove(1);
-//console.log(list.get(1));
+console.log(list.get(1)); 
+list.remove(1);
+console.log(list.get(1));
