2c7bac0600a09bbef97aac3a02afcb31294cec56
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-11 15:11:59 +0300

    Create 07-hes.js

---
+++ 18.JS-Advanced-Classes-Exercise/Classes - exercises/07-hes.js
@@ -0,0 +1,20 @@
+class Hex{
+    constructor(value){
+        this.value = value;
+
+    }
+
+
+    toString(){
+
+    }
+}
+
+let FF =new Hex(255);
+console.log(FF.toString());
+FF.valueOf()+1==256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString()==='0xF');
+console.log(FF.parse('AAA'));
