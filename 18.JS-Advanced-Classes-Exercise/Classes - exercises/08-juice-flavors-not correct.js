94f39ed1b3acf414ec25a542175425d6ec69a7a9
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-12 22:27:24 +0300

    classes exercise
    task 8 juices flaivors - v2

---
+++ 18.JS-Advanced-Classes-Exercise/Classes - exercises/08-juice-flavors-not correct.js
@@ -0,0 +1,51 @@
+function juiceFlavors(array){
+    let result = new Map;
+
+    for (const inputJuice of array) {
+        let [juiceName, quantity] = inputJuice.split(' => ');
+        quantity = Number(quantity)
+
+         if(result.has(juiceName)){
+            let current = Number(result.get(juiceName))
+            current += quantity
+            result.set(juiceName, current);
+
+        }else{
+            result.set(juiceName, quantity);
+
+        }
+
+        
+        
+    }
+    function bottles(juice, q){
+
+        if((q / 1000 )> 1){
+            let current =Math.floor(q / 1000)
+            console.log(`${juice} => ${current}`)
+        }else{
+            return q
+
+
+        }
+
+    }
+   
+result.forEach((el,x) => bottles(x, el));
+
+}
+
+juiceFlavors(['Orange => 2000',
+'Peach => 1432',
+'Banana => 450',
+'Peach => 600',
+'Strawberry => 549']);
+
+console.log('...................');
+
+juiceFlavors(['Kiwi => 234',
+'Pear => 2345',
+'Watermelon => 3456',
+'Kiwi => 4567',
+'Pear => 5678',
+'Watermelon => 6789'])
---
+++ 18.JS-Advanced-Classes-Exercise/Classes - exercises/08-juice-flavors_v2.js
@@ -0,0 +1,19 @@
+function juiceFlavors(array){
+  let juicesAmount = new Map;
+  
+}
+
+juiceFlavors(['Orange => 2000',
+'Peach => 1432',
+'Banana => 450',
+'Peach => 600',
+'Strawberry => 549']);
+
+console.log('...................');
+
+juiceFlavors(['Kiwi => 234',
+'Pear => 2345',
+'Watermelon => 3456',
+'Kiwi => 4567',
+'Pear => 5678',
+'Watermelon => 6789'])
