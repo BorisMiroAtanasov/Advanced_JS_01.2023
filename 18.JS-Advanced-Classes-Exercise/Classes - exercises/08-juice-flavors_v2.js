157f63bdc6841426f828d4b7f500cab047b5bfab
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-12 23:01:32 +0300

    Update 08-juice-flavors_v2.js

--- 18.JS-Advanced-Classes-Exercise/Classes - exercises/08-juice-flavors_v2.js
+++ 18.JS-Advanced-Classes-Exercise/Classes - exercises/08-juice-flavors_v2.js
@@ -1,21 +1,35 @@
 function juiceFlavors(juiceArr){
   let juicesAmount = new Map();
-  let bottles = new Map();
+  let juiceBottles = new Map();
   for (let index = 0; index < juiceArr.length; index++) {
     let[juicesName, amount] = juiceArr[index].split(' => ');
     amount = Number(amount);
-    
-    
-    
+
+    if(!juicesAmount.has(juicesName)){
+        juicesAmount.set(juicesName,0)
+    }
+    let totalAmout = juicesAmount.get(juicesName) + amount;
+    if(totalAmout > 1000) {
+        if(!juiceBottles.has(juicesName)){
+            juicesAmount.set(juicesName,0)
+
+    }
+    let newBottles = Math.trunc(totalAmout / 1000);
+    let totalBottles = juiceBottles.get(juicesName) + newBottles;
+    juiceBottles.set(juicesName,totalBottles)
+
   }
+  juicesAmount.set(juicesName,(totalAmout % 1000))
 
 }
+return [...juiceBottles].map(([key, value]) => `${key} => ${value}`).join('\n')
+}
 
-juiceFlavors(['Orange => 2000',
+console.log(juiceFlavors(['Orange => 2000',
 'Peach => 1432',
 'Banana => 450',
 'Peach => 600',
-'Strawberry => 549']);
+'Strawberry => 549']));
 
 console.log('...................');
 
