ddf96bd37d1c35d42ca80e256d8f47d22ab660fc
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-14 23:05:10 +0300

    Exam prep
    Art Gallery

---
+++ 22.Exam preparation/Exam prep - 14.02.2022/Art galery/02-art-gallery.js
@@ -0,0 +1,17 @@
+class ArtGallery{
+    constructor(creator){
+        this.creator = creator;
+        this.possibleArticles  = {"picture":200,"photo":50,"item":250};
+        this.listOfArticles = [];
+        this.guests  = guests;
+    
+    }
+    
+}
+
+
+
+const artGallery = new ArtGallery('Curtis Mayfield');
+console.log(artGallery.addArticle('picture', 'Mona Liza', 3));
+console.log(artGallery.addArticle('Item', 'Ancient vase', 2));
+console.log(artGallery.addArticle('PICTURE', 'Mona Liza', 1));
