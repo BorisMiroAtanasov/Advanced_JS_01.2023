a08c4645c238f7877eabc7d738b17e00581283fb
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-18 22:53:11 +0300

    02.Camping
    update

---
+++ 22.Exam preparation/Exam prep - 11.02.2022/02. Camping_solution _papazov/02.camping.js
@@ -0,0 +1,35 @@
+class SummerCamp{
+    constructor(organizer, location){
+        this.organizer = organizer;
+        this.location = location;
+        this.priceForTheCamp  = {"child": 150, "student": 300, "collegian": 500};
+        this.listOfParticipants = [];
+
+    }
+
+    registerParticipant (name, condition, money){
+        if(!this.priceForTheCamp[condition]){
+            throw new Error(`Unsuccessful registration at the camp.`)
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
