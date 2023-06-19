43bff896e921b75f43f51ee3513f05589bc3dbfb
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-19 22:46:54 +0300

    02 - camping
    Update

--- 22.Exam preparation/Exam prep - 11.02.2022/02. Camping_solution _papazov/02.camping.js
+++ 22.Exam preparation/Exam prep - 11.02.2022/02. Camping_solution _papazov/02.camping.js
@@ -1,57 +1,52 @@
-class SummerCamp{
-    constructor(organizer, location){
-        this.organizer = organizer;
-        this.location = location;
-        this.priceForTheCamp  = {"child": 150, "student": 300, "collegian": 500};
-        this.listOfParticipants = [];
+class SummerCamp {
+  constructor(organizer, location) {
+    this.organizer = organizer;
+    this.location = location;
+    this.priceForTheCamp = { child: 150, student: 300, collegian: 500 };
+    this.listOfParticipants = [];
+  }
 
+  registerParticipant(name, condition, money) {
+    if (!this.priceForTheCamp[condition]) {
+      throw new Error(`Unsuccessful registration at the camp.`);
     }
 
-    registerParticipant (name, condition, money){
-        if(!this.priceForTheCamp[condition]){
-            throw new Error(`Unsuccessful registration at the camp.`)
-        }
+    let participant = {
+      name,
+      condition,
+      power: 100,
+      wins: 0,
+    };
 
-        let participant = {name,
-             condition, 
-             power:100,
-             wins:  0,
-            }
-
-     
-        
-
-        if(this.listOfParticipants.some(x => x.name == name)) {
-            return `The ${name} is already registered at the camp.`
-            
-        }else if ( this.priceForTheCamp[condition] > money){
-            return `The money is not enough to pay the stay at the camp.`
-           
-        }else{
-            this.listOfParticipants.push(participant);
-            return `The ${name} was successfully registered.`
-
-        }
-        
-
+    if (this.listOfParticipants.some((x) => x.name == name)) {
+      return `The ${name} is already registered at the camp.`;
+    } else if (this.priceForTheCamp[condition] > money) {
+      return `The money is not enough to pay the stay at the camp.`;
+    } else {
+      this.listOfParticipants.push(participant);
+      return `The ${name} was successfully registered.`;
     }
-
+  }
+  unregisterParticipant(name) {
+    if (!this.listOfParticipants.some((x) => x.name == name)) {
+      throw new Error(`The ${name} is not registered in the camp.`);
+    }else{
+        this.listOfParticipants = this.listOfParticipants.filter(x => x.name != name)
+        return `The ${name} removed successfully.`
+    }
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
+// const summerCamp = new SummerCamp(
+//   "Jane Austen",
+//   "Pancharevo Sofia 1137, Bulgaria"
+// );
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
 
 const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
- console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
 console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
-console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
-console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
+console.log(summerCamp.unregisterParticipant("Petar"));
+console.log(summerCamp.unregisterParticipant("Petar Petarson"));
