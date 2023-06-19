2956ad9eee9172cea81b666a496445df58fa1915
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-19 23:38:10 +0300

    02 Camping
    Update

--- 22.Exam preparation/Exam prep - 11.02.2022/02. Camping_solution _papazov/02.camping.js
+++ 22.Exam preparation/Exam prep - 11.02.2022/02. Camping_solution _papazov/02.camping.js
@@ -22,17 +22,48 @@ class SummerCamp {
       return `The ${name} is already registered at the camp.`;
     } else if (this.priceForTheCamp[condition] > money) {
       return `The money is not enough to pay the stay at the camp.`;
-    } else {
-      this.listOfParticipants.push(participant);
-      return `The ${name} was successfully registered.`;
     }
+    this.listOfParticipants.push(participant);
+    return `The ${name} was successfully registered.`;
   }
   unregisterParticipant(name) {
     if (!this.listOfParticipants.some((x) => x.name == name)) {
       throw new Error(`The ${name} is not registered in the camp.`);
-    }else{
-        this.listOfParticipants = this.listOfParticipants.filter(x => x.name != name)
-        return `The ${name} removed successfully.`
+    } else {
+      this.listOfParticipants = this.listOfParticipants.filter(
+        (x) => x.name != name
+      );
+      return `The ${name} removed successfully.`;
+    }
+  }
+  timeToPlay(typeOfGame, participant1, participant2) {
+    let player1 = this.listOfParticipants.find((x) => x.name == participant1);
+
+    if (!player1) {
+      throw new Error(`Invalid entered name/s.`);
+    }
+
+    if (typeOfGame == "Battleship") {
+      player1.power += 20;
+      return `The ${player1.name} successfully completed the game ${typeOfGame}.`;
+    } else if (typeOfGame === "WaterBalloonFights") {
+      let player2 = this.listOfParticipants.find((x) => x.name == participant2);
+
+      if (!player2) {
+        throw new Error(`Invalid entered name/s.`);
+      }
+      if (player1.condition != player2.condition) {
+        throw new Error(`Choose players with equal condition.`);
+      }
+      if (player1.power > player2.power) {
+        player1.wins++;
+        return `The ${player1.name} is winner in the game ${typeOfGame}.`;
+      } else if (player1.power < player2.power) {
+        player2.wins++;
+        return `The ${player2.name} is winner in the game ${typeOfGame}.`;
+      } else {
+        return `There is no winner.`;
+      }
     }
   }
 }
@@ -46,7 +77,24 @@ class SummerCamp {
 // console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
 // console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
 
-const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+// const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.unregisterParticipant("Petar"));
+// console.log(summerCamp.unregisterParticipant("Petar Petarson"));
+
+const summerCamp = new SummerCamp(
+  "Jane Austen",
+  "Pancharevo Sofia 1137, Bulgaria"
+);
 console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
-console.log(summerCamp.unregisterParticipant("Petar"));
-console.log(summerCamp.unregisterParticipant("Petar Petarson"));
+console.log(summerCamp.timeToPlay("Battleship", "Petar Petarson"));
+console.log(summerCamp.registerParticipant("Sara Dickinson", "child", 200));
+//console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Sara Dickinson"));
+console.log(summerCamp.registerParticipant("Dimitur Kostov", "student", 300));
+console.log(
+  summerCamp.timeToPlay(
+    "WaterBalloonFights",
+    "Petar Petarson",
+    "Dimitur Kostov"
+  )
+);
