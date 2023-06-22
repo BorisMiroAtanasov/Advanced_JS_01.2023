a00bfb21768d50f2e384c7b5b9b2d7cca90f9c63
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-22 22:44:39 +0300

    03.Book selection
    solution start

---
+++ 22.Exam preparation/Exam prep - 11.02.2022/03.BookSelection/03-book-selection.test.js
@@ -0,0 +1,15 @@
+const {expect, assert} = require ("chai")
+const {isGenreSuitable, isItAffordable, suitableTitles} = require ('./03-book-selection');
+
+describe("Tests Book Selection", function() {
+     describe("isGenreSuitable", function() {
+ 
+   
+         it("sda", function() {
+            assert.equal(1,1)
+         });
+      });
+ 
+   
+     
+ });
