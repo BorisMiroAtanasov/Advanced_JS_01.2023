16984eee90b4053cc29986a768adb8d7bf1ff79b
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-22 22:14:05 +0300

    Update 02-garden.js

--- 22.Exam preparation/Exam prep  - 13.10.2022/02-garden.js
+++ 22.Exam preparation/Exam prep  - 13.10.2022/02-garden.js
@@ -36,7 +36,7 @@ class Garden {
     currentPlant.ripe = true;
     currentPlant.quantity += Number(quantity)
 
-    if(currentPlant.quantity = 1 ){
+    if(currentPlant.quantity === 1 ){
       return `${quantity} ${plantName} has successfully ripened.`
     }else if (currentPlant.quantity > 1 ){
       return `${quantity} ${plantName}s have successfully ripened.`
@@ -65,14 +65,26 @@ class Garden {
 
     });
     this.plants.splice(1,index);                  // this.plants.filter( x => x.plantName !== plantName)
-    this.spaceAvailable -= Number(currentPlant.spaceRequired) 
+    this.spaceAvailable += Number(currentPlant.spaceRequired) 
    
     return `The ${plantName} has been successfully harvested.`
 
   }
 
   generateReport(){
-    
+    let result = []
+    let resultStorage = []
+     this.storage.forEach(x => resultStorage.push(`${x.plantName} (${x.quantity})`))
+     this.plants
+    .sort((a,b) => a.plantName.localeCompare(b.plantName))
+    .forEach(x => result.push(x.plantName));
+    let storageFinalResult = this.storage.length === 0 ? `Plants in storage: The storage is empty.` : `Plants in storage: ${resultStorage.join(', ')}`
+
+
+// return this.storage
+    return `The garden has ${ this.spaceAvailable } free space left.\nPlants in the garden: ${result.join(', ')}\n${storageFinalResult}`
+
+  
   }
 
 
@@ -94,11 +106,20 @@ class Garden {
 
 
 
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 200));
+// console.log(myGarden.addPlant('raspberry', 10));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.harvestPlant('apple'));
+// console.log(myGarden.harvestPlant('olive'));
+
 const myGarden = new Garden(250)
 console.log(myGarden.addPlant('apple', 20));
 console.log(myGarden.addPlant('orange', 200));
 console.log(myGarden.addPlant('raspberry', 10));
 console.log(myGarden.ripenPlant('apple', 10));
 console.log(myGarden.ripenPlant('orange', 1));
-console.log(myGarden.harvestPlant('apple'));
-console.log(myGarden.harvestPlant('olive'));
+console.log(myGarden.harvestPlant('orange'));
+console.log(myGarden.generateReport());
