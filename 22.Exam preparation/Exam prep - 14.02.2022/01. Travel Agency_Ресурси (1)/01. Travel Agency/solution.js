a15942e2ee39f270ceb1225b4c67e58564b2fd5b
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-24 14:11:26 +0300

    01.Travel Agency
    Update solution

---
+++ 22.Exam preparation/Exam prep - 14.02.2022/01. Travel Agency_Ресурси (1)/01. Travel Agency/solution.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', solution);
+
+function solution() {
+  const fullName = document.getElementById(`fname`);
+  const email = document.getElementById(`email`);
+  const phoneNumber = document.getElementById(`phone`);
+  const address = document.getElementById(`address`);
+  const postCode = document.getElementById(`code`);
+  const submitBtn = document.getElementById('submitBTN')
+  const editBtn = document.getElementById('editBTN');
+  const continueBtn = document.getElementById('continueBTN');
+
+  
+
+
+
+
+
+
+
+
+
+}
