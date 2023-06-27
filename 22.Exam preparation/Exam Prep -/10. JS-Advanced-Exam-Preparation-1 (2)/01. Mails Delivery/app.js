b99788f6f647e0b55b98cea4a015ac95d2125351
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-27 23:46:15 +0300

    Update app.js

--- 22.Exam preparation/Exam Prep -/10. JS-Advanced-Exam-Preparation-1 (2)/01. Mails Delivery/app.js
+++ 22.Exam preparation/Exam Prep -/10. JS-Advanced-Exam-Preparation-1 (2)/01. Mails Delivery/app.js
@@ -1,12 +1,34 @@
 function solve() {
-    const replacmentInputElement = document.getElementById('recipientName');
-    const titleInputElenet = document.getElementById('title');
+    const rcipientInputElement = document.getElementById('recipientName');
+    const titleInputElemet = document.getElementById('title');
     const massageInputElement = document.getElementById('message');
     const addBtn = document.getElementById('add');
     const resetBtn = document.getElementById('reset');
     const listElement = document.querySelector('.list-mails');
     const listElementUl = document.getElementById('list');
+
+    addBtn.addEventListener('click', (event) =>{
+        event.preventDefault();
+
+    if(rcipientInputElement.value === '' || titleInputElemet ==='' || massageInputElement ==='' ){
+        return
+    };
+
+    let textAreaLi = document.createElement('li');
+    let headTitle = document.createElement('h4');
+    let headRecipient= document.createElement('h4');
+    let massage = document.createElement('span');
+
+    let listAction = document.createElement('div');
+
+
     
+
+
+
+
+    });
+
 
 
 
