76ea5634e6f53eb1c7cf6925eafc199d5f97b5cc
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-06-27 23:19:42 +0300

    Update

--- 22.Exam preparation/Exam Prep -/10. JS-Advanced-Exam-Preparation-1 (2)/01. Mails Delivery/app.js
+++ 22.Exam preparation/Exam Prep -/10. JS-Advanced-Exam-Preparation-1 (2)/01. Mails Delivery/app.js
@@ -1,4 +1,14 @@
 function solve() {
+    const replacmentInputElement = document.getElementById('recipientName');
+    const titleInputElenet = document.getElementById('title');
+    const massageInputElement = document.getElementById('message');
+    const addBtn = document.getElementById('add');
+    const resetBtn = document.getElementById('reset');
+    const listElement = document.querySelector('.list-mails');
+    const listElementUl = document.getElementById('list');
+    
+
+
 
 }
 solve()
