2b48fcf5c04c535f0e58841126872296da0820da
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-07-29 09:46:11 +0300

    Library collection
    update

--- 22.Exam preparation/Exam prep - 13.02.2023/11. JS-Advanced-Exam-Preparation-2/02. Library Collection/libraryCollection.js
+++ 22.Exam preparation/Exam prep - 13.02.2023/11. JS-Advanced-Exam-Preparation-2/02. Library Collection/libraryCollection.js
@@ -1,3 +1,42 @@
 class LibraryCollection {
-// TODO...
+    constructor(capacity){
+        this.capacity = capacity;
+        this.books = []
+        
+    }
+
+    addBook (bookName, bookAuthor){
+        let newBook = {
+            bookName,
+            bookAuthor,
+            payed: false,
+        }
+       
+        if( this.books.length >= this.capacity ){
+            return `Not enough space in the collection.`
+        }else{
+            this.books.push(this.newBook);
+            return `The ${bookName}, with an author ${bookAuthor}, collect.`
+
+        }
+
+        
+    }
+
+    payBook( bookName ){
+        
+    }
+
+
+
 }
+
+// const library = new LibraryCollection(2)
+// console.log(library.addBook('In Search of Lost Time', 'Marcel Proust'));
+// console.log(library.addBook('Don Quixote', 'Miguel de Cervantes'));
+// console.log(library.addBook('Ulysses', 'James Joyce'));
+
+const library = new LibraryCollection(2)
+library.addBook('In Search of Lost Time', 'Marcel Proust');
+console.log(library.payBook('In Search of Lost Time'));
+console.log(library.payBook('Don Quixote'));
